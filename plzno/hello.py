38302f09c142cf3f42f6e96199042ead7edf04a7
kamazzcha <ismailismailov8942@gmail.com>
2020-09-03 16:46:51 +0500

    not dagerous for your phone

---
+++ plzno/hello.py
@@ -0,0 +1,8 @@
+#Don't copy my proect bitch
+from colorama import init
+from colorama import Fore, Back, Style
+init()
+
+print(Fore.GREEN)
+while 1 == 1:
+	print("You have Been Hacked by @kamazzcha (instagram)")
